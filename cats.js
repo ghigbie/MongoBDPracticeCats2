985fa0b69316179d19658aa3622e07d908cd49d0
ghigbie <gehigbie@gmail.com>
2017-02-21 06:59:34 +0000

    added cats schema

--- cats.js
+++ cats.js
@@ -1,2 +1,8 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/cat_app");
+
+var catSchema = new monoose.Schema({
+    name: String,
+    age: Number,
+    temperament: String
+});
