773c48b4ac305754768e4b77e0071881e6598e1e
ghigbie <gehigbie@gmail.com>
2017-02-21 22:27:13 +0000

    added callback function

--- cats.js
+++ cats.js
@@ -12,8 +12,17 @@ var catSchema = new mongoose.Schema({ //this defines a pattern for our data
 //we have all the methods associated with the 
 var Cat = mongoose.modle("Cat", catSchema);
 
-new Cat({
+var george = new Cat({
     name: "George",
     age: 11,
     temperament: "Grouchy"
+});
+
+george.save(function(err, cat){
+    if(err){
+        console.log("SOMETHIGN WENT WRONG");
+    }else{
+        console.log("WE JUST SAVED A CAT TO THE DB");
+        console.log(cat);
+    }
 });
