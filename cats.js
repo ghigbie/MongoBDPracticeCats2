d7fdd16aa61fad58e5722425849e6f7f9918469f
ghigbie <gehigbie@gmail.com>
2017-02-21 22:22:24 +0000

    added comments to explain the cat db

--- cats.js
+++ cats.js
@@ -1,8 +1,15 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/cat_app");
+mongoose.connect("mongodb://localhost/cat_app"); //if cat app does not exist, it will created it.
 
-var catSchema = new monoose.Schema({
-    name: String,
-    age: Number,
-    temperament: String
+//need to define what a cat looks like
+var catSchema = new mongoose.Schema({ //this defines a pattern for our data
+   name: String,
+   age: Number,
+   temperament: String,
 });
+
+//we saved the cat Schema to a model
+//we have all the methods associated with the 
+var Cat = mongoose.modle("Cat", catSchema);
+
+
